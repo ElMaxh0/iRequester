7afa3e65f66c79f30e46d195c6f469eb7f6d8620
Alexandre Picinato Scherer <83470126+ElMaxh0@users.noreply.github.com>
2021-11-16 23:49:02 -0300

    Add files via upload

---
+++ iRequests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[18]:
+
+
+import requests 
+headers={
+    'Accept': '*/*',
+    'User-Agent':'request',
+}
+destino = "URL"
+i = 1
+while (i <= 500):
+    req = requests.get(destino , headers=headers)
+    print ('Requisição numero ', i , 'ResponseCode',req.status_code)
+    #print (req.text)
+    i=i+1
+
+
+# In[ ]:
+
+
+
+
